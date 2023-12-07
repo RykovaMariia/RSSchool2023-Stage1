11b7e28336b9370d186bde5ed9ec6f41ec549795
Mariia Rykova <maryrykova24@gmail.com>
2023-12-07 22:20:35 +0400

    feat: add slider js

--- coffee-house/js/slider.js
+++ coffee-house/js/slider.js
@@ -1,103 +1,153 @@
-const positions = [, 'position_1', 'position_2'];
-const imgs = document.querySelectorAll('.slide__image');
-const controls = document.querySelectorAll('.control');
-let setId;
-let setIdd;
+const positions = [, "position_1", "position_2"];
+const controls = document.querySelectorAll(".control");
 
+let startId;
+let contId;
+let checkedId;
+let position = 0;
+let timeline = 49;
+let value;
 
-  //Slider
-    
-    startSlide();
-    stopAndContinueScroll();
+//Slider
+start();
 
-    // Arrow
-    arrowRightClickHandler();
-    arrowLeftClickHandler();
+// Arrow
+arrowRightClickHandler();
+arrowLeftClickHandler();
+mouseMoveHandler();
 
+//Auto scroll
+function start() {
+  checked(position, 49);
 
-function startSlide() {
-    setId = setInterval(() => {
-        scrollRight();
-        autofillControl();
-    }, 5000)
-}
- 
-function stopAndContinueScroll() {
-imgs.forEach(img => {
-    img.addEventListener('mouseenter', () => {
-        clearInterval(setId);
-    });
-    img.addEventListener('mouseleave', () => {
-        startSlide();
-    });
-})
+  startId = setInterval(() => {
+    position = scrollRight();
+    value = controls[position].querySelector("progress").value;
+    checked(position, 49);
+  }, 5000);
+
 }
 
-const autofillControl = () => {
-    let position = determinePosition();
+function cont(timeline) {
+  checked(position, 49);
 
-    controls.forEach(control => control.classList.remove('control_checked'));
-    controls.forEach((control, index) => {
-    if(index == position){
+  contId = setTimeout(() => {
+    clearChecked();
+    position = scrollRight();
+    start();
+  }, timeline);
+}
 
-        control.classList.add('control_checked');
+//Autofill
+function checked(position, time) {
+  checkedId = setInterval(() => {
+    if (value <= 100) {
+      value++;
+    } else {
+      value = 0;
     }
-})
-
-
+    controls[position].querySelector("progress").value = value;
+  }, time);
 }
 
-function arrowRightClickHandler() {
-  document.querySelector(".slider__arrow:last-child ").addEventListener("click", () => {
-    clearInterval(setId);
-    scrollRight();
-    startSlide();
-  })
-};
+//Clear
+function clearChecked() {
+  clearInterval(checkedId);
+  controls.forEach((el) => (el.querySelector("progress").value = 0));
+}
 
-function arrowLeftClickHandler () {
-    document.querySelector(".slider__arrow:first-child ").addEventListener("click", () => {
-        clearInterval(setId);
-      let position = determinePosition();
+function clearCheck() {
+  clearInterval(checkedId);
+  value = controls[position].querySelector("progress").value;
+  console.log(value);
+}
+
+function clearStart() {
+  clearInterval(startId);
+}
+
+function clearCont() {
+    clearInterval(contId);
+  }
+
+//Arrow
+function arrowRightClickHandler() {
+  document
+    .querySelector(".slider__arrow:last-child ")
+    .addEventListener("click", () => {
+      clearStart();
+      scrollRight();
+      position = determinePosition();
+      value = 0;
+      start();
+    });
+}
+
+function arrowLeftClickHandler() {
+  document
+    .querySelector(".slider__arrow:first-child ")
+    .addEventListener("click", () => {
+      clearStart();
+      clearChecked();
+      position = determinePosition();
       if (position > 0) {
-          removeClassPosition();
-          addClassPosition(position - 1);
+        removeClassPosition();
+        position -= 1;
       } else {
         removeClassPosition();
-        addClassPosition(2);
+        position = 2;
       }
-      startSlide();
-    })
+      addClassPosition(position);
+      value = 0;
+      start();
+    });
+}
+
+function mouseMoveHandler() {
+    document.querySelectorAll(".slide__image").forEach((img) => {
+    img.addEventListener("mouseenter", () => {
+      clearStart();
+      clearCont(); 
+      clearCheck();
+    });
+    img.addEventListener("mouseleave", () => {
+      position = determinePosition();
+      timeline = (100 - value) * 50;
+      cont(timeline);
+    });
+  });
+}
+
+//
+const scrollRight = () => {
+  position = determinePosition();
+  clearChecked();
+  if (position < 2) {
+    removeClassPosition();
+    addClassPosition(++position);
+  } else {
+    removeClassPosition();
+    position = 0;
+  }
+  return position;
 };
 
-const scrollRight = () => {
-let position = determinePosition();
-    if (position < 2) {
-        removeClassPosition();
-        addClassPosition(position + 1);
-
-    } else {
-        removeClassPosition();
-    }
-}
-
 const determinePosition = () => {
-    let position;
-    document.querySelector(".slider__line").classList.forEach(clazz => {
-        if (positions.includes(clazz)) {
-            position = positions.indexOf(clazz);
-        } else {
-            position = 0;
-        }
-    })
-    return position;
-}
+  let position;
+  document.querySelector(".slider__line").classList.forEach((clazz) => {
+    if (positions.includes(clazz)) {
+      position = positions.indexOf(clazz);
+    } else {
+      position = 0;
+    }
+  });
+  return position;
+};
 
 const removeClassPosition = () => {
   document.querySelector(".slider__line").classList.remove(...positions);
 };
 
 const addClassPosition = (position) => {
-    document.querySelector(".slider__line").classList.add(`position_${position}`);
+  document.querySelector(".slider__line").classList.add(`position_${position}`);
 };
-

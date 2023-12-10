2851a46a18b3bfe7a7af9698b28343b6fd871e71
Mariia Rykova <maryrykova24@gmail.com>
2023-12-10 21:04:09 +0400

    feat: add animation open modal

--- coffee-house/js/burger.js
+++ coffee-house/js/burger.js
@@ -11,6 +11,7 @@ function buttonBurgerClickHandler() {
       closeButtonBurger();
       addScroll();
     } else {
+      window.scrollTo(0, 0);
       openBurgerMenu();
       openButtonBurger();
       removeScroll();

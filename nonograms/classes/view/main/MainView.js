6be8699a2df77b07a90ea723150dc51afba4fdd4
Mariia Rykova <maryrykova24@gmail.com>
2024-01-31 01:52:46 +0400

    feat: add adaptive

--- nonograms/classes/view/main/MainView.js
+++ nonograms/classes/view/main/MainView.js
@@ -1,3 +1,4 @@
+import { CreatorElement } from "../../utils/CreatorElement.js";
 import { BaseView } from "../BaseView.js";
 import { GameView } from "./game/GameView.js";
 import { MenuView } from "./menu/MenuView.js";
@@ -20,5 +21,33 @@ export class MainView extends BaseView {
 
     const score = new ScoreView();
     this.viewElement.appendElement(score.getHTMLElement());
+
+    this.appendMenuButton(game, menu.getHTMLElement());
+    this.appendScoreButton(game, score.getHTMLElement());
+  }
+
+  appendMenuButton(game, menuEl) {
+    const menu = new CreatorElement("button", ["button","button_menu"], "", () =>
+      this.cbMenu(menuEl)
+    );
+    game.viewElement.appendElement(menu.getElement());
+  }
+
+  cbMenu(menuEl) {
+    menuEl.classList.toggle("menu_opened");
+  }
+
+  appendScoreButton(game, scoreEl) {
+    const button = new CreatorElement(
+      "button",
+      ["button", "button_score"],
+      "",
+      () => this.cbScoreButton(scoreEl)
+    );
+    game.viewElement.appendElement(button.getElement());
+  }
+
+  cbScoreButton(scoreEl) {
+    scoreEl.classList.toggle("score_opened");
   }
 }

cf3b33f625505a72c271e4e23522bde9108a7be9
Mariia Rykova <maryrykova24@gmail.com>
2024-02-02 01:11:59 +0400

    fix: fix theme

--- nonograms/classes/app.js
+++ nonograms/classes/app.js
@@ -1,5 +1,4 @@
 import { MainView } from "./view/main/MainView.js";
-import { ModalView } from "./view/modal/ModalView.js";
 
 export class App {
   constructor() {
@@ -8,10 +7,6 @@ export class App {
 
   createView() {
     const main = new MainView();
-    
-
     document.body.prepend(main.getHTMLElement());
-}
-
-
+  }
 }
